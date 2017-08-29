bb35575a4376af0cd8430bf74e6b8595d7780140
IGD2017 <tun.onizuka@gmail.com>
2017-08-29 12:35:01 +0700

    Project Version1 Success

--- app/src/main/java/phansa/phaiboon/actionview/MainActivity.java
+++ app/src/main/java/phansa/phaiboon/actionview/MainActivity.java
@@ -5,27 +5,63 @@ import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private String urlString = "http://www.inspectorrta.org/IGD2017/";
+    private int anInt = 0;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                myIntentToAcitonview();
-            }
-        }, 3000);
+        if (savedInstanceState!=null) {
+            myIntentToAcitonview();
+        }
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+
+        myLoop();
 
 
 
     }  //Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void myDelay() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                myLoop();
+            }
+        }, 200);
+    }
+
+    private void myLoop() {
+        //Todo
+        //Increase Inetger
+        anInt += 10;
+
+        //check integer
+        if (anInt == 100) {
+            myIntentToAcitonview();
+        }
+        progressBar.setProgress(anInt);
+
+        myDelay();
+
+
+    }
 
     private void myIntentToAcitonview() {
         Intent intent = new Intent(Intent.ACTION_VIEW);
